8e28a34d337ed73ce9cf9828e14df1950fbc62c8
dev glord <maximianobatista1@gmail.com>
2023-08-21 23:27:32 +0100

    Adicionando alguns projetos

---
+++ QuartoProjeto/QuartoProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+namespace Projeto
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            n--;
+            Console.WriteLine(n);
+        }
+    }
+}
