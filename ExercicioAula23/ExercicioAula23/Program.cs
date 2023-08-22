6739e3929f203a1f0a14db22affc5031230c7b23
dev glord <maximianobatista1@gmail.com>
2023-08-22 23:38:01 +0100

    Adicionando alguns projetos vistos na aula e tbm exercicios feitos

---
+++ ExercicioAula23/ExercicioAula23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace NonoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome_completo = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int numero_quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco_produtos = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimo_nome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2],CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Nome completo: {nome_completo}");
+            Console.WriteLine($"Número de Quartos: {numero_quartos}");
+            Console.WriteLine($"Preço de um produto: R${preco_produtos.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Último nome: {ultimo_nome}");
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Altura: {altura.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Projeto6/Projeto6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace projeto
+{
+    class program 
+    {
+        static void Main(string[] args)
+        {
+            double n3 = (double) 10 / 8;
+            
+            Console.WriteLine(n3);
+            
+        }
+    }
+}
