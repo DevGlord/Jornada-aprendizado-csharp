8c24103468325a419209c851fbe9319f040c4472
dev glord <maximianobatista1@gmail.com>
2023-08-24 17:27:20 +0100

    Adicionando projetos e exercicios do curso

---
+++ ExercicioEstruturaCondicional1/ExercicioEstruturaCondicional1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioEstruturaCondicional1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 > 0)
+            {
+                Console.WriteLine($"O número digitado {n1} é Positivo.");
+            }
+
+            else
+            {
+                Console.WriteLine($"O número digitado {n1} é Negativo.");
+            }
+        }
+    }
+}
---
+++ ExercicioEstruturalCondicional2/ExercicioEstruturalCondicional2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioEstruturalCondicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 % 2 == 0)
+            {
+                Console.WriteLine($"O número inteiro digitado {n1} é Par.");
+            }
+            
+            else
+            {
+                Console.WriteLine($"O número inteiro digitado {n1} é Impar.");
+            }
+        }
+    }
+}
