914364ec2b56a120bc6f1d922c633409273e033d
devglord <maximianobatista1@gmail.com>
2023-09-13 12:59:55 +0100

    Adicionando um exercicio que o instrutor deixou para fazer

---
+++ Classes-atributos-metodos-MembrosEstaticos/Projeto7/Conversao.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Projeto7
+{
+    class Conversao
+    {
+        public static double Convertor(double cotacao, double total_pagar) {
+            double total_pago = ((total_pagar * 0.06) + total_pagar) * cotacao;
+            return total_pago;
+        } 
+    }
+}
---
+++ Classes-atributos-metodos-MembrosEstaticos/Projeto7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Projeto7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dolar?");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar?");
+            double total_comprar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double conversao = Conversao.Convertor(cotacao, total_comprar);
+
+            Console.WriteLine($"Valor a ser pago em real: {conversao.ToString("F2",CultureInfo.InvariantCulture)}R$");
+        }
+    }
+}
